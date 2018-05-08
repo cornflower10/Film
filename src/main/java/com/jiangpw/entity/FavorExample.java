5a2e3476102c0fa25540dd6ab6c49665cc4b0e87
jiangpw <jiangpw.com@gmail.com>
2018-05-08 14:03:50 +0800

    因为表名设计数据库关键字，修改表名

---
+++ src/main/java/com/jiangpw/entity/FavorExample.java
@@ -0,0 +1,501 @@
+package com.jiangpw.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class FavorExample {
+    protected String orderByClause;
+
+    protected boolean distinct;
+
+    protected List<Criteria> oredCriteria;
+
+    public FavorExample() {
+        oredCriteria = new ArrayList<Criteria>();
+    }
+
+    public void setOrderByClause(String orderByClause) {
+        this.orderByClause = orderByClause;
+    }
+
+    public String getOrderByClause() {
+        return orderByClause;
+    }
+
+    public void setDistinct(boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    public boolean isDistinct() {
+        return distinct;
+    }
+
+    public List<Criteria> getOredCriteria() {
+        return oredCriteria;
+    }
+
+    public void or(Criteria criteria) {
+        oredCriteria.add(criteria);
+    }
+
+    public Criteria or() {
+        Criteria criteria = createCriteriaInternal();
+        oredCriteria.add(criteria);
+        return criteria;
+    }
+
+    public Criteria createCriteria() {
+        Criteria criteria = createCriteriaInternal();
+        if (oredCriteria.size() == 0) {
+            oredCriteria.add(criteria);
+        }
+        return criteria;
+    }
+
+    protected Criteria createCriteriaInternal() {
+        Criteria criteria = new Criteria();
+        return criteria;
+    }
+
+    public void clear() {
+        oredCriteria.clear();
+        orderByClause = null;
+        distinct = false;
+    }
+
+    protected abstract static class GeneratedCriteria {
+        protected List<Criterion> criteria;
+
+        protected GeneratedCriteria() {
+            super();
+            criteria = new ArrayList<Criterion>();
+        }
+
+        public boolean isValid() {
+            return criteria.size() > 0;
+        }
+
+        public List<Criterion> getAllCriteria() {
+            return criteria;
+        }
+
+        public List<Criterion> getCriteria() {
+            return criteria;
+        }
+
+        protected void addCriterion(String condition) {
+            if (condition == null) {
+                throw new RuntimeException("Value for condition cannot be null");
+            }
+            criteria.add(new Criterion(condition));
+        }
+
+        protected void addCriterion(String condition, Object value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value));
+        }
+
+        protected void addCriterion(String condition, Object value1, Object value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        public Criteria andIdIsNull() {
+            addCriterion("id is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIsNotNull() {
+            addCriterion("id is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdEqualTo(Integer value) {
+            addCriterion("id =", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotEqualTo(Integer value) {
+            addCriterion("id <>", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThan(Integer value) {
+            addCriterion("id >", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdGreaterThanOrEqualTo(Integer value) {
+            addCriterion("id >=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThan(Integer value) {
+            addCriterion("id <", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdLessThanOrEqualTo(Integer value) {
+            addCriterion("id <=", value, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdIn(List<Integer> values) {
+            addCriterion("id in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotIn(List<Integer> values) {
+            addCriterion("id not in", values, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdBetween(Integer value1, Integer value2) {
+            addCriterion("id between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andIdNotBetween(Integer value1, Integer value2) {
+            addCriterion("id not between", value1, value2, "id");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountIsNull() {
+            addCriterion("likecount is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountIsNotNull() {
+            addCriterion("likecount is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountEqualTo(Integer value) {
+            addCriterion("likecount =", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountNotEqualTo(Integer value) {
+            addCriterion("likecount <>", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountGreaterThan(Integer value) {
+            addCriterion("likecount >", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountGreaterThanOrEqualTo(Integer value) {
+            addCriterion("likecount >=", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountLessThan(Integer value) {
+            addCriterion("likecount <", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountLessThanOrEqualTo(Integer value) {
+            addCriterion("likecount <=", value, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountIn(List<Integer> values) {
+            addCriterion("likecount in", values, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountNotIn(List<Integer> values) {
+            addCriterion("likecount not in", values, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountBetween(Integer value1, Integer value2) {
+            addCriterion("likecount between", value1, value2, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andLikecountNotBetween(Integer value1, Integer value2) {
+            addCriterion("likecount not between", value1, value2, "likecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountIsNull() {
+            addCriterion("unlikecount is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountIsNotNull() {
+            addCriterion("unlikecount is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountEqualTo(Integer value) {
+            addCriterion("unlikecount =", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountNotEqualTo(Integer value) {
+            addCriterion("unlikecount <>", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountGreaterThan(Integer value) {
+            addCriterion("unlikecount >", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountGreaterThanOrEqualTo(Integer value) {
+            addCriterion("unlikecount >=", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountLessThan(Integer value) {
+            addCriterion("unlikecount <", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountLessThanOrEqualTo(Integer value) {
+            addCriterion("unlikecount <=", value, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountIn(List<Integer> values) {
+            addCriterion("unlikecount in", values, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountNotIn(List<Integer> values) {
+            addCriterion("unlikecount not in", values, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountBetween(Integer value1, Integer value2) {
+            addCriterion("unlikecount between", value1, value2, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andUnlikecountNotBetween(Integer value1, Integer value2) {
+            addCriterion("unlikecount not between", value1, value2, "unlikecount");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeIsNull() {
+            addCriterion("createtime is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeIsNotNull() {
+            addCriterion("createtime is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeEqualTo(Date value) {
+            addCriterion("createtime =", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeNotEqualTo(Date value) {
+            addCriterion("createtime <>", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeGreaterThan(Date value) {
+            addCriterion("createtime >", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeGreaterThanOrEqualTo(Date value) {
+            addCriterion("createtime >=", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeLessThan(Date value) {
+            addCriterion("createtime <", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeLessThanOrEqualTo(Date value) {
+            addCriterion("createtime <=", value, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeIn(List<Date> values) {
+            addCriterion("createtime in", values, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeNotIn(List<Date> values) {
+            addCriterion("createtime not in", values, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeBetween(Date value1, Date value2) {
+            addCriterion("createtime between", value1, value2, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andCreatetimeNotBetween(Date value1, Date value2) {
+            addCriterion("createtime not between", value1, value2, "createtime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeIsNull() {
+            addCriterion("updatetime is null");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeIsNotNull() {
+            addCriterion("updatetime is not null");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeEqualTo(Date value) {
+            addCriterion("updatetime =", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeNotEqualTo(Date value) {
+            addCriterion("updatetime <>", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeGreaterThan(Date value) {
+            addCriterion("updatetime >", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeGreaterThanOrEqualTo(Date value) {
+            addCriterion("updatetime >=", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeLessThan(Date value) {
+            addCriterion("updatetime <", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeLessThanOrEqualTo(Date value) {
+            addCriterion("updatetime <=", value, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeIn(List<Date> values) {
+            addCriterion("updatetime in", values, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeNotIn(List<Date> values) {
+            addCriterion("updatetime not in", values, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeBetween(Date value1, Date value2) {
+            addCriterion("updatetime between", value1, value2, "updatetime");
+            return (Criteria) this;
+        }
+
+        public Criteria andUpdatetimeNotBetween(Date value1, Date value2) {
+            addCriterion("updatetime not between", value1, value2, "updatetime");
+            return (Criteria) this;
+        }
+    }
+
+    public static class Criteria extends GeneratedCriteria {
+
+        protected Criteria() {
+            super();
+        }
+    }
+
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+    }
+}
---
+++ src/main/java/com/jiangpw/mapper/FavorMapper.java
@@ -0,0 +1,21 @@
+package com.jiangpw.mapper;
+
+import com.jiangpw.entity.Favor;
+import com.jiangpw.entity.FavorExample;
+import java.util.List;
+
+public interface FavorMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Favor record);
+
+    int insertSelective(Favor record);
+
+    List<Favor> selectByExample(FavorExample example);
+
+    Favor selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Favor record);
+
+    int updateByPrimaryKey(Favor record);
+}
